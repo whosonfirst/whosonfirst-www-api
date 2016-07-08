d88936b9b351a267487927aa2ae885f3594f6691
thisisaaronland <whosonfirst@mapzen.com>
2016-07-08 18:34:50 +0000

    just go straight to the API

--- www/index.php
+++ www/index.php
@@ -1,6 +1,13 @@
 <?php
 	include('include/init.php');
 
+	if ($GLOBALS['cfg']['user']){
+
+		$redir = $GLOBALS['cfg']['abs_root_url'] . "api/";
+		header("location: $redir");
+		exit();
+	}
+
 	$GLOBALS['smarty']->display('page_index.txt');
 	exit();
 
