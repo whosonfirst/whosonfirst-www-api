e3499916721d34f8ca8d428499f78a0454e9dc14
thisisaaronland <whosonfirst@mapzen.com>
2016-07-26 03:13:37 +0000

    flesh out machinetag methods - probably break everything :D :D :D

--- www/include/lib_whosonfirst_machinetags.php
+++ www/include/lib_whosonfirst_machinetags.php
@@ -10,17 +10,40 @@
 
 	function whosonfirst_machinetags_get_namespaces($args){
 
+		return whosonfirst_machinetags_hierarchies($args);
 	}
 
 	########################################################################
 
 	function whosonfirst_machinetags_get_predicates($args){
 
+		return whosonfirst_machinetags_hierarchies($args);
 	}
 
 	########################################################################
 
 	function whosonfirst_machinetags_get_values($args){
+
+		return whosonfirst_machinetags_hierarchies($args);
+	}
+
+	########################################################################
+
+	function whosonfirst_machinetags_hierarchies($args){
+
+		$aggrs = array('hierarchies' => array(
+			'terms' => 'field' => 'machinetags', 'size' => 0
+		));
+
+		list($include_filter, $exclude_filter) = machinetags_elasticsearch_hierarchy_query_filters($args);
+
+		if ($include_filter){
+			$aggrs['hierarchies']['terms']['include'] = $include_filter;
+		}
+
+		if ($exlude_filter){
+			$aggrs['hierarchies']['terms']['exclude'] = $exclude_filter;
+		}
 
 	}
 
