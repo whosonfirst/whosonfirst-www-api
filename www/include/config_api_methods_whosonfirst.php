2d00619e3e5e45b6c6d2824f9b52f23234418c25
thisisaaronland <whosonfirst@mapzen.com>
2017-01-26 21:15:29 +0000

    enable_pip for pip

--- www/include/config_api_methods_whosonfirst.php
+++ www/include/config_api_methods_whosonfirst.php
@@ -137,7 +137,7 @@
 		'whosonfirst.places.getByLatLon' => array(
 			"description" => "Lookup Who's On First records for a latitude and longitude",
 			"documented" => 1,
-			"enabled" => $GLOBALS['cfg']['enable_feature_spatial'],
+			"enabled" => $GLOBALS['cfg']['enable_feature_pip'],
 			"paginated" => 0,
 			"library" => "api_whosonfirst_places",
                         "parameters" => array(
