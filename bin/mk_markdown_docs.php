749316b042964817103f3f4401e3df19ff4ea68b
thisisaaronland <whosonfirst@mapzen.com>
2017-03-06 22:27:14 +0000

    per issue #19 stub out a tool to render MD docs

---
+++ bin/mk_markdown_docs.php
@@ -0,0 +1,49 @@
+<?php
+
+	include("init_local.php");
+
+	loadlib("api");
+	loadlib("api_methods");
+
+	ksort($GLOBALS['cfg']['api']['methods']);
+
+	foreach ($GLOBALS['cfg']['api']['methods'] as $method_name => $details){
+
+		$details['name'] = $method_name;
+
+		if (! api_methods_can_view_method($details, 0)){
+			continue;
+		}
+
+		$parts = explode(".", $method_name);
+		array_pop($parts);
+
+		$method_prefix = $parts[0];
+		$method_class = implode(".", $parts);
+
+		if (! is_array($method_classes[$method_class])){
+
+			$method_classes[$method_class] = array(
+				'methods' => array(),
+				'prefix' => $method_prefix,
+			);
+		}
+
+		$method_classes[$method_class]['methods'][] = $details;
+		$method_names[] = $details['name'];
+	}
+
+	foreach ($method_classes as $class_name => $ignore){
+		usort($method_classes[$class_name]['methods'], function($a, $b) {
+			return strcmp($a['name'], $b['name']);
+		});
+	}
+
+	$GLOBALS['smarty']->assign_by_ref("method_classes", $method_classes);
+
+	$formats = $GLOBALS['cfg']['api']['formats'];
+	$GLOBALS['smarty']->assign_by_ref("response_formats", $formats);
+
+	$md = $GLOBALS['smarty']->fetch("markdown_mapzen_api_docs.txt");
+
+	echo $md;
