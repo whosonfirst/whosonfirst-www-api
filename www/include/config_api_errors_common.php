85676081eb37d653141a6abe88751537bfcd2d16
thisisaaronland <whosonfirst@mapzen.com>
2017-03-09 14:57:44 +0000

    disable documentation for a bunch of error methods that don't make sense for MZ API

--- www/include/config_api_errors_common.php
+++ www/include/config_api_errors_common.php
@@ -31,118 +31,144 @@
 
 		"450" => array(
 			"message" => "Unknown error.",
+			"documented" => 1,
 		),
 
 		# query
 
 		"452" => array(
 			"message" => "Insufficient parameters.",
+			"documented" => 1,
 		),
 
 		"453" => array(
 			"message" => "Missing parameter.",
+			"documented" => 1,
 		),
 
 		"454" => array(
 			"message" => "Invalid parameter.",
+			"documented" => 1,
 		),
 
 		# uploads
 
 		"455" => array(
-			"message" => "Invalid upload response."
+			"message" => "Invalid upload response.",
+			"documented" => 0,
 		),
 
 		"456" => array(
-			"message" => "Missing upload body."
+			"message" => "Missing upload body.",
+			"documented" => 0,
 		),
 
 		"457" => array(
-			"message" => "Upload exceeded maximum filesize."
+			"message" => "Upload exceeded maximum filesize.",
+			"documented" => 0,
 		),
 
 		"458" => array(
-			"message" => "Invalid mime-type."
+			"message" => "Invalid mime-type.",
+			"documented" => 0,
 		),
 
 		# users
 
 		"460" => array(
 			"message" => "Invalid user.",
+			"documented" => 0,
 		),
 
 		"461" => array(
 			"message" => "User is disabled.",
+			"documented" => 0,
 		),
 
 		"462" => array(
 			"message" => "User is deleted.",
+			"documented" => 0,
 		),
 
 		# API keys
 
 		"478" => array(
 			"message" => "Insufficient permissions for this API key.",
+			"documented" => 1,
 		),
 
 		"479" => array(
 			"message" => "Invalid access token for this API key.",
+			"documented" => 0,
 		),
 
 		"481" => array(
 			"message" => "Unauthorized host for this API key.",
+			"documented" => 1,
 		),
 
 		"482" => array(
 			"message" => "API key not configured for use with this method.",
+			"documented" => 1,
 		),
 
 		"483" => array(
 			"message" => "Invalid API key.",
+			"documented" => 1,
 		),
 
 		"484" => array(
 			"message" => "API key missing.",
+			"documented" => 1,
 		),
 
 		# access tokens
 
 		"490" => array(
 			"message" => "Access token has insuffient permissions.",
+			"documented" => 0,
 		),
 
 		"491" => array(
 			"message" => "Access token is expired.",
+			"documented" => 0,
 		),
 
 		"492" => array(
 			"message" => "Access token is disabled.",
+			"documented" => 0,
 		),
 
 		"493" => array(
 			"message" => "Invalid access token.",
+			"documented" => 0,
 		),
 
 		"494" => array(
 			"message" => "Access token missing.",
+			"documented" => 0,
 		),
 
 		# API methods
 
 		"497" => array(
-			"message" => "Output format is disallowed for this API method."
+			"message" => "Output format is disallowed for this API method.",
+			"documented" => 1,
 		),
 
 		"498" => array(
-			"message" => "API method is disabled."
+			"message" => "API method is disabled.",
+			"documented" => 1,
 		),
 
 		"499" => array(
-			"message" => "API method not found."
+			"message" => "API method not found.",
+			"documented" => 1,
 		),
 
 		"512" => array(
-			"message" => "Something we tried to do didn't work. This is our fault, not yours."
+			"message" => "Something we tried to do didn't work. This is our fault, not yours.",
+			"documented" => 1,
 		),
 
 	);
