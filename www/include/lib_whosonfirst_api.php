6bc70ff2fbc828f3b34faab25674b82265e240e2
thisisaaronland <whosonfirst@mapzen.com>
2017-03-20 20:25:14 +0000

    add libary to call the API itself

---
+++ www/include/lib_whosonfirst_api.php
@@ -0,0 +1,43 @@
+<?php
+
+	$GLOBALS['cfg']['whosonfirst_api_endpoint'] = "https://whosonfirst-api.dev.mapzen.com";
+
+	########################################################################
+
+	loadlib("http");
+
+	########################################################################
+
+	function whosonfirst_api_call($method, $args=array()){
+
+		$rsp = whosonfirst_execute_method($method, $args);
+
+		if (! $rsp['ok']){
+			return $rsp;
+		}
+
+		$data = json_decode($rsp["body"], "as hash");
+
+		if (! $data){
+			return array("ok" => 0, "error" => "Failed to parse response");
+		}
+
+		return array("ok" => 1, "data" => $data);
+	}
+
+	########################################################################
+
+	function whosonfirst_execute_method($method, $args=array()){
+
+		$url = $GLOBALS['cfg']['whosonfirst_api_endpoint'];
+		$args["method"] = $method;
+
+		$headers = array();
+
+		return http_post($url, $args, $headers);
+	}
+	
+	########################################################################
+
+	# the end
+	
