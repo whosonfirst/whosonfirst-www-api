161af2985277e2620a2533478045ebf8d5d08dee
thisisaaronland <whosonfirst@mapzen.com>
2017-03-27 01:02:55 +0000

    redirect to WOF docs if not logged in

--- www/index.php
+++ www/index.php
@@ -1,6 +1,11 @@
 <?php
 	include('include/init.php');
 
+	if (! $GLOBALS['cfg']['user']){
+		header("location: https://mapzen.com/documentation/wof/");
+		exit();
+	}
+
 	$GLOBALS['smarty']->display('page_index.txt');
 	exit();
 
