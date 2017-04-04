3014e8dc27eb2e8346c811fbe92ed2e226b787a5
thisisaaronland <whosonfirst@mapzen.com>
2017-04-04 22:51:10 +0000

    update specs for sources and placetypes; add /ping

--- www/index.php
+++ www/index.php
@@ -1,10 +1,10 @@
 <?php
 	include('include/init.php');
 
-	if (! $GLOBALS['cfg']['user']){
-		header("location: https://mapzen.com/documentation/wof/");
-		exit();
-	}
+#	if (! $GLOBALS['cfg']['user']){
+#		header("location: https://mapzen.com/documentation/wof/");
+#		exit();
+#	}
 
 	$GLOBALS['smarty']->display('page_index.txt');
 	exit();
---
+++ www/ping.php
@@ -0,0 +1,8 @@
+<?php
+	include('include/init.php');
+
+	header("Content-Type: text/plain");
+
+	echo "PONG";
+	exit();
+?>
