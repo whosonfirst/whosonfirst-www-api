82c38130bb71707250efc609fa436ffadb482eb5
thisisaaronland <whosonfirst@mapzen.com>
2017-08-31 00:39:09 +0000

    stub out the placetype page; redirect from ID/nearby to /nearby/ID

--- www/id.php
+++ www/id.php
@@ -16,6 +16,12 @@
 		error_404();
 	}
 
+	if (get_isset("nearby")){
+		$url = whosonfirst_places_nearby_url_for_place($place);
+		header("location: {$url}");
+		exit();
+	}
+
 	$parent_id = $place["wof:parent_id"];
 
 	if ($parent_id != -1){
@@ -24,7 +30,7 @@
 	}
 
 	$GLOBALS['smarty']->assign_by_ref("place", $place);
-	$GLOBALS['smarty']->display("page_id.txt", $place);
+	$GLOBALS['smarty']->display("page_id.txt");
 
 	exit();
 ?>
