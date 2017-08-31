48dbf4b22c6ffec1c95e3dc59e72ab9f64c9f3d4
thisisaaronland <whosonfirst@mapzen.com>
2017-08-31 01:13:39 +0000

    handle multiple parents on /id pages

--- www/id.php
+++ www/id.php
@@ -2,6 +2,7 @@
 
 	include("include/init.php");
 	loadlib("whosonfirst_places");
+	loadlib("whosonfirst_placetypes");
 
 	if (! get_isset("id")){
 		header("location: {$GLOBALS['cfg']['abs_root_url']}");
@@ -24,9 +25,38 @@
 
 	$parent_id = $place["wof:parent_id"];
 
-	if ($parent_id != -1){
+	if ($parent_id > -1){
 		$parent = whosonfirst_places_get_by_id($parent_id);
 		$place["wof:parent"] = $parent;
+	}
+
+	else {
+	
+		$pt = $place["wof:placetype"];
+		$hiers = $place["wof:hierarchy"];
+
+		$parent_ids = array();
+		$parent_key = null;
+
+		foreach (whosonfirst_placetypes_parents($pt) as $ppt){
+
+			$k = "{$ppt}_id";
+
+			if (isset($hiers[0][$k])){
+				$parent_key = $k;
+				break;
+			}
+		}
+
+		foreach ($hiers as $hier){
+			$parent_ids[] = $hier[$parent_key];
+		}
+
+		$rsp = whosonfirst_places_get_by_id_multi($parent_ids);
+
+		if ($rsp['ok']){
+			$place["wof:parents"] = $rsp["rows"];
+		}
 	}
 
 	$GLOBALS['smarty']->assign_by_ref("place", $place);
