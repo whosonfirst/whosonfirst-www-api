94c2006963437eda85c04ad4e7f703a027d5982d
Dan Phiffer <dan@phiffer.org>
2017-09-28 15:17:36 -0400

    :mag: show nearby results as a table

--- www/javascript/mapzen.places.nearby.init.js
+++ www/javascript/mapzen.places.nearby.init.js
@@ -1,5 +1,50 @@
 window.addEventListener("load", function load(event){
 
+	var no_results = document.querySelector('#no-results');
+	var loading_results = document.querySelector('#loading-results');
+	var num_results = document.querySelector('#num-results');
+	var results_table = document.querySelector('#search-results');
+	var results_tbody = results_table.querySelector('tbody');
+	var result_template = document.querySelector('#result-template');
+	var abs_root_url = document.body.getAttribute("data-abs-root-url");
+
+	function add_geojson_to_results_table(geojson){
+		for (var i = 0; i < geojson.features.length; i++){
+			var feature = geojson.features[i];
+			var props = feature.properties;
+			var tr = result_template.cloneNode(true);
+			results_tbody.appendChild(tr);
+
+			var esc_id = htmlspecialchars(props['wof:id']);
+			tr.querySelector('.id').innerHTML = esc_id;
+
+			var esc_name = htmlspecialchars(props['wof:name']);
+			var esc_href = htmlspecialchars(abs_root_url + 'id/' + props['wof:id'] + '/');
+			tr.querySelector('.name').innerHTML = esc_name;
+			tr.querySelector('.name').setAttribute('href', esc_href);
+
+			var esc_pt = htmlspecialchars(props['wof:placetype']);
+			tr.querySelector('.placetype').innerHTML = esc_pt;
+			tr.className = 'results-item';
+
+			var country_code = props['iso:country'];
+			if (typeof whosonfirst_countries[country_code] != 'undefined'){
+				var country = whosonfirst_countries[country_code];
+				var country_id = htmlspecialchars(country['wof:id']);
+				var country_name = htmlspecialchars(country['wof:name']);
+				var country_link = '<a href="' + abs_root_url + 'id/' + country_id + '">' + country_name + '</a>';
+				tr.querySelector('.country').innerHTML = country_link;
+			}
+		}
+		results_table.className = 'table';
+		loading_results.className = 'hidden';
+		no_results.className = 'hidden';
+
+		var num = results_tbody.querySelectorAll('tr').length;
+		num_results.innerHTML = num.toLocaleString() + ' results';
+		num_results.className = '';
+	}
+
 	var cb = function(map){
 
 		L.Mapzen.hash({
@@ -7,6 +52,16 @@ window.addEventListener("load", function load(event){
 		});
 
 		var nearby_layer;
+
+		var nearby_clear = function(){
+			if (nearby_layer){
+				nearby_layer.remove(map);
+			}
+			no_results.className = '';
+			results_table.className = 'hidden';
+			results_tbody.innerHTML = '';
+			num_results.className = 'hidden';
+		};
 
 		var nearby = function(){
 
@@ -21,7 +76,7 @@ window.addEventListener("load", function load(event){
 				"latitude": lat,
 				"longitude": lon,
 				"per_page": 500,
-				"extras": "geom:,lbl:",
+				"extras": "geom:,iso:country,lbl:",
 			};
 
 			var on_page = function(rsp){
@@ -60,15 +115,17 @@ window.addEventListener("load", function load(event){
 				};
 
 				nearby_layer = mapzen.places.map.add_geojson_to_map(map, feature_collection);
+				add_geojson_to_results_table(feature_collection);
 			};
 
 			var on_error = function(rsp) {
 				console.error(rsp);
 			};
 
-			if (nearby_layer){
-				nearby_layer.remove(map);
-			}
+			nearby_clear();
+
+			loading_results.className = '';
+			no_results.className = 'hidden';
 
 			mapzen.places.api.call_paginated(method, args, on_page, on_error);
 		};
@@ -104,12 +161,12 @@ window.addEventListener("load", function load(event){
 		map.on({
 			locationfound: function() {
 				var css = btn.className + '';
-				btn.className = css.replace(' disabled', '');
+				btn.className = css.replace('disabled', '');
 				nearby();
 			},
 			locationerror: function() {
 				var css = btn.className + '';
-				btn.className = css.replace(' disabled', '');
+				btn.className = css.replace('disabled', '');
 			}
 		});
 	};
