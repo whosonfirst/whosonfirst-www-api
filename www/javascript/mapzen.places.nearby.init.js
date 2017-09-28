5ba2eb8fda1afb7c75762a552a29503dad96d3d9
Dan Phiffer <dan@phiffer.org>
2017-09-28 11:34:59 -0400

    :dizzy: get basic nearby page working
    re: #83

--- www/javascript/mapzen.places.nearby.init.js
+++ www/javascript/mapzen.places.nearby.init.js
@@ -1,85 +1,87 @@
 window.addEventListener("load", function load(event){
 
-    var cb = function(map){
+	var cb = function(map){
 
-	L.Mapzen.hash({
-	    map: map
-	});
+		L.Mapzen.hash({
+			map: map
+		});
 
-	var nearby_layer;
+		var nearby_layer;
 
-	var nearby = function(){
+		var nearby = function(){
 
-	    var center = map.getCenter();
+			var center = map.getCenter();
 
-	    var lat = center.lat;
-	    var lon = center.lng;
+			var lat = center.lat;
+			var lon = center.lng;
 
-	    var method = "whosonfirst.places.getNearby";
+			var method = "whosonfirst.places.getNearby";
 
-	    var args = {
-		"latitude": lat,
-		"longitude": lon,
-		"per_page": 200,
-		"extras": "geom:",
-	    };
+			var args = {
+				"latitude": lat,
+				"longitude": lon,
+				"per_page": 500,
+				"extras": "geom:",
+			};
 
-	    var on_success = function(rsp){
+			var on_page = function(rsp){
 
-		var features = [];
+				var features = [];
 
-		var places = rsp["places"];
-		var count_places = places.length;
+				var places = rsp["places"];
+				var count_places = places.length;
 
-		for (var i=0; i < count_places; i++){
+				for (var i=0; i < count_places; i++){
 
-		    var place = places[i];
+					var place = places[i];
 
-		    var name = place["wof:name"];
+					var name = place["wof:name"];
 
-		    var lat = place["geom:latitude"];
-		    var lon = place["geom:longitude"];
+					var lat = place["geom:latitude"];
+					var lon = place["geom:longitude"];
 
-		    var coords = [ lon, lat ];
+					var coords = [ lon, lat ];
 
-		    var geom = {
-			"type": "Point",
-			"coordinates": coords,
-		    };
+					var geom = {
+						"type": "Point",
+						"coordinates": coords,
+					};
 
-		    var props = place;
+					var props = place;
 
-		    var feature = {
-			"type": "Feature",
-			"geometry": geom,
-			"properties": props,
-		    };
+					var feature = {
+						"type": "Feature",
+						"geometry": geom,
+						"properties": props,
+					};
 
-		    features.push(feature);
-		}
+					features.push(feature);
+				}
 
-		var feature_collection = {
-		    "type": "FeatureCollection",
-		    "features": features,
+				var feature_collection = {
+					"type": "FeatureCollection",
+					"features": features,
+				};
+
+				nearby_layer = mapzen.places.map.add_geojson_to_map(map, feature_collection);
+			};
+
+			var on_error = function(rsp) {
+				console.error(rsp);
+			};
+
+			if (nearby_layer){
+				nearby_layer.remove(map);
+			}
+
+			mapzen.places.api.call_paginated(method, args, on_page, on_error);
 		};
 
-		nearby_layer = mapzen.places.map.add_geojson_to_map(map, feature_collection);
-	    };
-	  
-	    mapzen.places.api.execute_method(method, args, function(rsp){
-		on_success(rsp);
-	    });
+		map.on("dragend", nearby);
+		map.on("zoomend", nearby);
 
-	    if (nearby_layer){
-		nearby_layer.remove(map);
-	    }
+		nearby();
 	};
-	
-	map.on("dragend", nearby);
-	map.on("zoomend", nearby);
 
-	nearby();
-    };
-
-    mapzen.places.map.draw_nearby_map("map", cb);
+	mapzen.places.map.draw_nearby_map("map", cb);
 });
