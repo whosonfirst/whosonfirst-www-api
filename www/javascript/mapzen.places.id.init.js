500bf443962a9892c4623d74ace16d23461dd86a
Dan Phiffer <dan@phiffer.org>
2017-10-05 16:36:06 -0400

    :wrench: default units / single line form / fit_bounds tweaks
    re: #93

--- www/javascript/mapzen.places.id.init.js
+++ www/javascript/mapzen.places.id.init.js
@@ -49,13 +49,24 @@ window.addEventListener("load", function load(event){
 			fetch(abs_path, onsuccess, onerror);
 		}
 
+		var units = 'metric';
+		if ("localStorage" in window &&
+		    localStorage.units){
+			units = localStorage.units;
+		}
+		else {
+			fetch('https://ip.dev.mapzen.com/?raw=1', function(rsp) {
+				units = get_default_units(rsp.country_id);
+			});
+		}
+
 		var go_geolocate_click = function(e){
 			e.preventDefault();
 			if ("geolocation" in navigator){
 				navigator.geolocation.getCurrentPosition(function(position) {
 					var lat = position.coords.latitude.toFixed(6);
 					var lon = position.coords.longitude.toFixed(6);
-					go_set_inputs(lat + ', ' + lon);
+					go_show_inputs(lat + ', ' + lon);
 				});
 			}
 			else {
@@ -63,7 +74,7 @@ window.addEventListener("load", function load(event){
 			}
 		};
 
-		var go_set_inputs = function(from, type){
+		var go_show_inputs = function(from, type){
 			if (type){
 				var select = document.getElementById('go-costing');
 				for (var i = 0; i < select.options.length; i++){
@@ -89,10 +100,10 @@ window.addEventListener("load", function load(event){
 			}
 
 			if (isNaN(lat) || isNaN(lon)){
-				go_set_inputs('', type);
+				go_show_inputs('', type);
 				return "Invalid latitude/longitude starting point.";
 			}
-			go_set_inputs(from, type);
+			go_show_inputs(from, type);
 			var from = L.latLng(lat, lon);
 
 			var costings = {
@@ -126,6 +137,9 @@ window.addEventListener("load", function load(event){
 				router: L.Mapzen.routing.router({
 					costing: costings[type]
 				}),
+				formatter: new L.Mapzen.routing.formatter({
+					units: units
+				}),
 				defaultErrorHandler: function(e){
 					if (e.error &&
 					    e.error.message){
@@ -153,8 +167,7 @@ window.addEventListener("load", function load(event){
 			return true;
 		};
 
-		document.getElementById('go-geolocate').addEventListener('click', go_geolocate_click, false);
-		document.getElementById('go-show-latlon').addEventListener('click', function(e){
+		var go_show_inputs_click = function(e){
 			e.preventDefault();
 			setTimeout(function(){
 				var from = document.getElementById('go-from');
@@ -165,8 +178,11 @@ window.addEventListener("load", function load(event){
 					from.select();
 				}
 			}, 0);
-			go_set_inputs();
-		}, false);
+			go_show_inputs();
+		};
+
+		document.getElementById('go-geolocate').addEventListener('click', go_geolocate_click, false);
+		document.getElementById('go-show-inputs').addEventListener('click', go_show_inputs_click, false);
 
 		var args = {};
 		var query = window.location.search.substr(1).split('&');
@@ -187,8 +203,65 @@ window.addEventListener("load", function load(event){
 				alert.className = 'alert alert-danger headroom';
 			}
 		}
+
+		// Adapted from https://mapzen.com/resources/projects/turn-by-turn/demo/demo.js
+
+		// Adjust padding for fitBounds()
+		// ==============================
+		//
+		// See this discussion: https://github.com/perliedman/leaflet-routing-machine/issues/60
+		// We override Leaflet's default fitBounds method to use our padding options by
+		// default. Thus, LRM calls fitBounds() as is. Additionally, any other scripts
+		// that call for fitBounds() can take advantage of the same padding behaviour.
+
+		var is_mobile = (window.innerWidth < 472);
+		var bounds_tl = is_mobile ? [15, 200] : [320, 30];
+		var bounds_br = is_mobile ? [15, 15] : [30, 30];
+
+		map.origFitBounds = map.fitBounds;
+		map.fitBounds = function (bounds, options) {
+			map.origFitBounds(bounds, {
+				// Left padding accounts for the narrative window.
+				// Top padding accounts for the floating section navigation bar.
+				// These conditions apply only when the viewport breakpoint is at
+				// desktop screens or higher. Otherwise, assume that the narrative
+				// window is not present, and that the section navigation is
+				// condensed, so less padding is required on mobile viewports.
+				paddingTopLeft: bounds_tl,
+				// Bottom and right padding accounts only for slight
+				// breathing room, in order to prevent markers from appearing
+				// at the very edge of maps.
+				paddingBottomRight: bounds_br,
+			});
+		};
+
+		// Adjust offset for panTo()
+		// ==============================
+		map.origPanTo = map.panTo;
+		// In LRM, coordinate is array format [lat, lng]
+		map.panTo = function (coordinate) {
+			var offset_x = Math.round((bounds_tl[0] - bounds_br[0]) / 2);
+			var offset_y = Math.round((bounds_tl[1] - bounds_br[1]) / 2);
+			var x = map.latLngToContainerPoint(coordinate).x - offset_x;
+			var y = map.latLngToContainerPoint(coordinate).y - offset_y;
+			var point = map.containerPointToLatLng([x, y]);
+			map.origPanTo(point);
+		};
 	};
 
 	mapzen.places.map.draw_place_map("map", cb);
+
+	function get_default_units(country_id){
+		var units = 'metric';
+		if (country_id == 85633793 || // USA
+		    country_id == 85632181 || // Myanmar
+		    country_id == 85632249){  // Liberia
+			units = 'imperial';
+		}
+		if ("localStorage" in window){
+			localStorage.units = units;
+		}
+		return units;
+	}
 
 });
