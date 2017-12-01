bf0e4ddb51e287dace25225d3402982b1fa4cd36
thisisaaronland <whosonfirst@mapzen.com>
2017-12-01 20:11:42 +0000

    indicate when a #brand has been superseded/supersedes

--- www/brand.php
+++ www/brand.php
@@ -18,13 +18,45 @@
 		error_404();
 	}
 
+	$supersedes = array();
+	$superseded_by = array();
+
+	if (is_array($brand["wof:superseded_by"])){
+
+		foreach ($brand["wof:superseded_by"] as $other_id){
+
+			$other_brand = whosonfirst_brands_get_by_id($other_id);
+			$superseded_by[] = $other_brand;		
+		}
+	}
+
+	if (is_array($brand["wof:supersedes"])){
+
+		foreach ($brand["wof:supersedes"] as $other_id){
+
+			$other_brand = whosonfirst_brands_get_by_id($other_id);
+			$supersedes[] = $other_brand;		
+		}
+	}
+
 	$more = array();
 
 	if ($p = get_int32("page")){
 		$more["page"] = $p;
 	}	
 
-	$rsp = whosonfirst_places_get_by_brand($brand, $more);
+	# if you're looking at this thinking... wait, WTF??
+	# then you'd be correct - it works but it's neither
+	# pretty nor does make much sense (beyond you know...
+	# working) - it's all still in flux and wrapped up with
+	# the way we're doing stuff in the API - it can (should?)
+	# all be changed... (20171202/thisisaaronland)
+
+	$filters = array(
+		array("term" => array("wof:brand_id" => $id ))
+	);
+
+	$rsp = whosonfirst_places_get_by_brand($brand, $filters, $more);
 
 	if (! $rsp["ok"]){
 		error_500();
@@ -34,6 +66,8 @@
 	$pagination = $rsp["pagination"];
 
 	$GLOBALS['smarty']->assign_by_ref("brand", $brand);
+	$GLOBALS['smarty']->assign_by_ref("superseded_by", $superseded_by);
+	$GLOBALS['smarty']->assign_by_ref("supersedes", $supersedes);
 
 	$GLOBALS['smarty']->assign_by_ref("places", $places);
 	$GLOBALS['smarty']->assign_by_ref("pagination", $pagination);
